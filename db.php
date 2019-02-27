8d42d82ab6ba45709da156818f6edb6d84f01e18
David Setim <timsedee@gmail.com>
2019-02-27 19:07:06 +0300

    Delete db.php

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("142.93.252.109","dsetim","Settim@2019","bet_tips_ke");
-if (mysqli_connect_errno()){
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
